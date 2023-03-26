8a522ec6822927f73eb448beb78779ee9e9615ef
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2023-03-25 18:56:44 -0600

    add calculations

--- index.php
+++ index.php
@@ -8,5 +8,30 @@
 </head>
 <body>
     
+<?php
+    // INPUT
+    $increase = 5299.73;
+    $pto = 115.074 + 39.385;
+
+    // CONSTANTS
+    $tithing = 0.1;
+    $offerings = 0.01;
+    $hours = 40;
+
+    // CALCULATIONS
+    $i = $increase;
+    $t = $increase * $tithing;
+    $f = $increase * $offerings;
+    $m = $increase * $offerings;
+    $p = $pto / $hours
+
+    // OUTPUT
+    echo ceil($i);
+    echo ceil($t);
+    echo ceil($f);
+    echo ceil($m);
+    echo floor($p);
+?>
+
 </body>
 </html>
